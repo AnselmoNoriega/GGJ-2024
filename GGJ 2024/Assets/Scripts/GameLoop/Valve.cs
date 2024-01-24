9746c0ad6e9705360d1c50d98836eb79220b04e5
AnselmoNoriega <anselmonoriega3@gmail.com>
2024-01-24 15:58:01 -0800

    visual changes

--- GGJ 2024/Assets/Scripts/GameLoop/Valve.cs
+++ GGJ 2024/Assets/Scripts/GameLoop/Valve.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Valve : MonoBehaviour
 {
+    [SerializeField] private Image[] _selectionImage; 
     private bool _rotateValveOption;
 
     public void Choices(bool choice)
     {
         _rotateValveOption = choice;
+        if(choice)
+        {
+            _selectionImage[0].color = Color.white;
+            _selectionImage[1].color = Color.red;
+        }
+        else
+        {
+            _selectionImage[1].color = Color.white;
+            _selectionImage[0].color = Color.red;
+        }
     }
 
     public void Rotate()
@@ -20,6 +32,11 @@ public class Valve : MonoBehaviour
 
     public bool ReturnChoice()
     {
+        foreach(var item in _selectionImage)
+        {
+            item.color = Color.white;
+        }
+
         bool choice = _rotateValveOption;
         _rotateValveOption = false;
         return choice;
