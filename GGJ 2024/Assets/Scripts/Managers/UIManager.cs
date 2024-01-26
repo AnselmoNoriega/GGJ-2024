1ac2c06caf77c557f31ac447e48c47ed41fb28ea
KebinDev <134655367+KebinDev@users.noreply.github.com>
2024-01-25 17:26:15 -0800

    Health Gauge Functionality

---
+++ GGJ 2024/Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _mainMenu;
+    
+    public void ButtonSceneLoader(string _scene)
+    {
+        SceneManager.LoadScene(_scene);
+    }
+
+    public void ButtonPanelActive(GameObject _panel)
+    {
+        if (_panel.activeInHierarchy == true)
+        {
+            _panel.SetActive(false);
+            _mainMenu.SetActive(true);
+        }
+        else
+        {
+            _panel.SetActive(true);
+            _mainMenu.SetActive(false);
+        }
+    }
+
+    public void ButtonExitGame()
+    {
+        Application.Quit();
+    }
+}
