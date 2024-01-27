3eb82007fed49356cd39f651d147e16498e2dee4
brightsteel-k <kmbrent@email.com>
2024-01-26 20:11:42 -0800

    Added camera pan functionality
    (Temporarily breaks game... working on restructuring UI after)

--- GGJ 2024/Assets/Scripts/Managers/CameraManager.cs
+++ GGJ 2024/Assets/Scripts/Managers/CameraManager.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    [SerializeField] private Vector2 _maxDistance;
-    [SerializeField] private float _sensitibity = 2.0f;
+    [SerializeField] private float _maxXAngle;
+    [SerializeField] private float _maxYAngle;
+    [SerializeField] private float _sensitivity = 0.1f;
 
-    [SerializeField] private Vector2 _screenMiddlePoint;
-
-    private float _rotationX = 0.0f;
-    private float _rotationY = 0.0f;
+    private Vector2 _screenMiddlePoint;
 
     private void Awake()
     {
@@ -18,20 +16,14 @@ public class CameraManager : MonoBehaviour
     }
     private void Update()
     {
-        Vector2 disFromCenter = (Vector2)Input.mousePosition - _screenMiddlePoint;
-
-        if( _maxDistance.x <= Mathf.Abs(disFromCenter.x))
+        if (Time.timeScale > 0)
         {
-            Vector3 currentRot = transform.rotation.eulerAngles;
-            float rotationAngleX = Mathf.Clamp(currentRot.y + disFromCenter.normalized.x *_sensitibity, -45, 45);
-            transform.rotation = Quaternion.Euler(currentRot.x, rotationAngleX, currentRot.z);
-        }
+            Vector2 disFromCenter = (Vector2)Input.mousePosition - _screenMiddlePoint;
 
-        //if( _maxDistance.y <= Mathf.Abs(disFromCenter.y))
-        //{
-        //    Vector3 currentRot = transform.rotation.eulerAngles;
-        //    float rotationAngleY = currentRot.x - disFromCenter.normalized.y *_sensitibity;
-        //    transform.rotation = Quaternion.Euler(rotationAngleY, currentRot.y, currentRot.z);
-        //}
+            Vector3 currentRot = transform.rotation.eulerAngles;
+            float rotationAngleX = Mathf.Clamp(-disFromCenter.y * _sensitivity, -_maxXAngle, _maxXAngle);
+            float rotationAngleY = Mathf.Clamp(disFromCenter.x * _sensitivity, -_maxYAngle, _maxYAngle);
+            transform.eulerAngles = new Vector3(rotationAngleX, rotationAngleY, currentRot.z);
+        }
     }
 }
