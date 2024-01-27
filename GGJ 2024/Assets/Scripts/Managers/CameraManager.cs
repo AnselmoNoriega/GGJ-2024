0f1da1589c256fd62bfb1e217142fbf5061dd738
brightsteel-k <kmbrent@email.com>
2024-01-27 11:25:12 -0800

    Modified Camera Manager
    Camera angle is no longer linearly dependent on the cursor's distance from the center of the screen--uses x^3 instead

--- GGJ 2024/Assets/Scripts/Managers/CameraManager.cs
+++ GGJ 2024/Assets/Scripts/Managers/CameraManager.cs
@@ -6,12 +6,12 @@ public class CameraManager : MonoBehaviour
 {
     [SerializeField] private float _maxXAngle;
     [SerializeField] private float _maxYAngle;
-    [SerializeField] private float _sensitivity = 0.1f;
 
     private Vector2 _screenMiddlePoint;
 
     private void Awake()
     {
+
         _screenMiddlePoint = new Vector2(Screen.width / 2, Screen.height / 2);
     }
     private void Update()
@@ -21,8 +21,8 @@ public class CameraManager : MonoBehaviour
             Vector2 disFromCenter = (Vector2)Input.mousePosition - _screenMiddlePoint;
 
             Vector3 currentRot = transform.rotation.eulerAngles;
-            float rotationAngleX = Mathf.Clamp(-disFromCenter.y * _sensitivity, -_maxXAngle, _maxXAngle);
-            float rotationAngleY = Mathf.Clamp(disFromCenter.x * _sensitivity, -_maxYAngle, _maxYAngle);
+            float rotationAngleX = Mathf.Clamp(-Mathf.Pow(disFromCenter.y / _screenMiddlePoint.y, 3), -_maxXAngle, _maxXAngle);
+            float rotationAngleY = Mathf.Clamp(Mathf.Pow(disFromCenter.x / _screenMiddlePoint.x, 3), -_maxYAngle, _maxYAngle);
             transform.eulerAngles = new Vector3(rotationAngleX, rotationAngleY, currentRot.z);
         }
     }
