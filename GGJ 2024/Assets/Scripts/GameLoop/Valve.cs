ceb34f91d35f19f1550124d8abe49d4a938324e9
AnselmoNoriega <anselmonoriega3@gmail.com>
2024-01-26 17:56:13 -0800

    gauge rotation cancel

--- GGJ 2024/Assets/Scripts/GameLoop/Valve.cs
+++ GGJ 2024/Assets/Scripts/GameLoop/Valve.cs
@@ -3,16 +3,27 @@ using UnityEngine.UI;
 
 public class Valve : MonoBehaviour
 {
-    [SerializeField] private Animator _animation;
+    [SerializeField] private GameObject _gauge;
     [SerializeField] private Animator _canisterAnimation;
+    private Animator _animation;
     private bool _rotateValveOption;
+
+    private void Awake()
+    {
+        _animation = _gauge.GetComponent<Animator>();
+    }
 
     public void Choices()
     {
-        if (!_rotateValveOption)
+        if (!_rotateValveOption && _gauge.transform.rotation.eulerAngles.z == 0)
         {
             _rotateValveOption = true;
             _animation.Play("ValveTurning");
+        }
+        else if(_gauge.transform.rotation.eulerAngles.z == 0)
+        {
+            _rotateValveOption = false;
+            _animation.Play("ValveTuningBack");
         }
     }
 
