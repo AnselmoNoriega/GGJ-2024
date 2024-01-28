47360b483fe2ac1c96e6969a1ac0b57f1fa89a5d
PureAether <marcvanniekerk84@gmail.com>
2024-01-27 18:09:51 -0800

    Cursor Manager Code

--- GGJ 2024/Assets/Scripts/Player/CursorClass.cs
+++ GGJ 2024/Assets/Scripts/Player/CursorClass.cs
@@ -4,9 +4,35 @@ using UnityEngine;
 
 public class CursorClass : MonoBehaviour
 {
-    [SerializeField] private Texture2D _cursor;
+    [Header("Cursor Normal")]
+    [SerializeField] private Texture2D _cursorNormal;
+    [SerializeField] private Vector2 _cursorNormalHotSpot;
+
+    [Header("Cursor Interactable")]
+    [SerializeField] private Texture2D _cursorInteractable;
+    [SerializeField] private Vector2 _cursorInteractableHotSpot;
+
+    [Header("Cursor Valve")]
+    [SerializeField] private Texture2D _cursorValve;
+    [SerializeField] private Vector2 _cursorValveHotSpot;
+
     void Start()
     {
-        Cursor.SetCursor(_cursor, Vector2.zero, CursorMode.Auto);
+        Cursor.SetCursor(_cursorNormal, Vector2.zero, CursorMode.Auto);
+    }
+
+    public void OnCursorInteractableEnter()
+    {
+        Cursor.SetCursor(_cursorInteractable, _cursorInteractableHotSpot, CursorMode.Auto);
+    }
+    
+    public void OnCursorValveEnter()
+    {
+        Cursor.SetCursor(_cursorValve, _cursorValveHotSpot, CursorMode.Auto);
+    }
+
+    public void ReturnCursorToNormal()
+    {
+        Cursor.SetCursor(_cursorNormal, _cursorNormalHotSpot, CursorMode.Auto);
     }
 }
